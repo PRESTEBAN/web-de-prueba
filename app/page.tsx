d4517cac92107a2fa223ba930c96772793e83f9f
PRESTEBAN <estebanpesantez22@gmail.com>
2026-04-04 20:18:05 -0500

    xd

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1>Hola, mi primera web con Next.js 🚀</h1>
-      <p>Estoy esperando mi cariñosa de 40 bro</p>
+      <p>NO QUIERO MAMAGUEVO</p>
     </main>
   );
 }
